c90edcb25a18b195d020c9af4380de7b757ae9dd
Jason Zhu <jasonzhu@Jasons-MacBook-Pro-3.local>
2020-04-10 13:47:08 -0400

    add description for R pacakges and add python installation in seir.rmd

--- coronaModel/R/sier_predict.R
+++ coronaModel/R/sier_predict.R
@@ -1,8 +1,16 @@
 #' Calculate quantile regerssion loss
 #'
-#' @param a1 value of y-X*beta
-#' @param a2 value of probability
-#' @return z x ( tau - 0)$ if z < 0 or $ z x (tau - 1)  otherwise
+#'@param pred_days number of days we want to predict for
+#' @param a1 value of alpha1
+#' @param a2 value of alpha2
+#' @param b value of beta
+#' @param s value of sigma
+#' @param g value of gmma
+#' @param E initial number of infected people with no symptom
+#' @param I initial number of confirmed infected people with symptom
+#' @param D initial number of death
+#' @param C initial number of recovered people
+#' @return The prediction i.e. confirmed, death, recovered based on the params for pred_days `(I, D, C)`
 #' @export
 sier_predict <- function(pred_days, a1, a2, b, s, g, E, I, D, C){
   pred.E <- c()
